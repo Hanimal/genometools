5dcded767ac8dc3150020af165d1f2df4aae93f3
Hannah <2rauterb@informatik.uni-hamburg.de>
2015-07-03 12:08:59 +0200

    f_score not testet

--- src/extended/f_score.h
+++
@@ -1,6 +0,0 @@
-#ifndef F_SCORE_H
-#define F_SCORE_H
-
-GtUword f_score(GtEncseq *encseq, GtUword r, GtUword k, GtUword *alpha_code, GtError *err);
-
-#endif
---
+++ src/tools/gt_scores.h
@@ -0,0 +1,6 @@
+#ifndef GT_SCORES_H
+#define GT_SCORES_H
+
+int calc_fscore(int argc, char *argv[]);
+
+#endif
