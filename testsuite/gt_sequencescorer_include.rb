1873b845d57c4a2b00967eb2cf0c3dd00d468ccd
Hannah <2rauterb@informatik.uni-hamburg.de>
2015-08-20 11:29:28 +0200

    testing

--- testsuite/gt_sequencescorer_include.rb
+++ testsuite/gt_sequencescorer_include.rb
@@ -1,8 +1,17 @@
 Name "gt_sequencescorer_check_gramdistance"
 Keywords "gt_sequencescorer qgram distance"
 Test do
-  run "#{$bin}gt scorer -qgram -s #{$testdata}sequencescorer_one.txt #{$testdata}sequencescorer_two.txt -q 4"
-  run "diff -B #{last_stdout} #{$testdata}sequencescorer_compare_q=4.out"
-  run "#{$bin}gt scorer -qgram -s #{$testdata}sequencescorer_one.txt #{$testdata}sequencescorer_two.txt -q 3"
-  run "diff -B #{last_stdout} #{$testdata}sequencescorer_compare_q=3.out"
+  run "#{$bin}gt scorer -qgram -s #{$testdata}EncodeTransProt7_One.txt #{$testdata}EncodeTransProt7_Two.txt -q 4"
+  run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt7_q4.out"
+  run "#{$bin}gt scorer -qgram -s #{$testdata}EncodeTransProt11_One.txt #{$testdata}EncodeTransProt11_Two.txt -q 5"
+  run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt11_q5.out"
+end 
+
+Name "gt_sequencescorer_check_fscore"
+Keywords "gt_sequencescorer fscore distance"
+Test do
+  run "#{$bin}gt scorer -fscore -s #{$testdata}EncodeTransProt7_One.txt #{$testdata}EncodeTransProt7_Two.txt -k 4"
+  run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt7_k4.out"
+  run "#{$bin}gt scorer -fscore -s #{$testdata}EncodeTransProt11_One.txt #{$testdata}EncodeTransProt11_Two.txt -k 5"
+  run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt11_k5.out"
 end 
