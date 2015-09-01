f5ff783c96dffc8eeeb0805f5c1830ee6975eddf
Hannah <2rauterb@informatik.uni-hamburg.de>
2015-09-01 09:40:12 +0200

    formatting

--- src/extended/scorematrix.c
+++ src/extended/scorematrix.c
@@ -8,9 +8,10 @@ void allocate_matrix(Scorematrix *smatrix)
 {
     GtUword i;
     smatrix->matrix = malloc(sizeof(GtUword*)*smatrix->dim);
-    *(smatrix->matrix)=malloc(((smatrix->dim*(smatrix->dim+1))>>1)*sizeof(GtUword));
+    *(smatrix->matrix) = malloc(((smatrix->dim*(smatrix->dim+1))>>1)
+                         *sizeof(GtUword));
 
-    for(i=1; i<smatrix->dim; i++)
+    for (i=1; i<smatrix->dim; i++)
         smatrix->matrix[i]=smatrix->matrix[i-1]+(smatrix->dim-(i-1));
 }
 
@@ -20,34 +21,34 @@ Scorematrix *read_score(FILE *fp)
     int tmp;
     smatrix->dim=0;
     GtUword j,
-            i = 0, 
+            i = 0,
             size = SIZE;
     char c,
          *s;
-    
-    s = malloc(sizeof(char)*SIZE); 
-    CHECK_READ(fread(&c, sizeof(char),1, fp),1);
+
+    s = malloc(sizeof(char)*SIZE);
+    CHECK_READ(fread(&c, sizeof char,1, fp),1);
     while (c == '#')
     {
         while (c != '\n')
-            CHECK_READ(fread(&c, sizeof(char),1, fp),1);
-        CHECK_READ(fread(&c, sizeof(char),1, fp),1);
+            CHECK_READ(fread(&c, sizeof char,1, fp),1);
+        CHECK_READ(fread(&c, sizeof char,1, fp),1);
     }
-    while(c != '\n')
+    while (c != '\n')
     {
-        if(i == size)
+        if (i == size)
         {
             size = size+SIZE;
             s = realloc(s, size);
         }
-        if(c != ' ')
+        if (c != ' ')
         {
             s[i] = c;
             i++;
         }
-        CHECK_READ(fread(&c, sizeof(char),1, fp),1);
+        CHECK_READ(fread(&c, sizeof char,1, fp),1);
     }
-    if(i == size)
+    if (i == size)
     {
         size+=1;
         s=realloc(s, size);
@@ -56,30 +57,30 @@ Scorematrix *read_score(FILE *fp)
     smatrix->order = s;
     smatrix->dim = i;
     allocate_matrix(smatrix);
-    
-    for(i = 0; i < smatrix->dim; i++)
+
+    for (i = 0; i < smatrix->dim; i++)
     {
         CHECK_READ(fscanf(fp,"%c",&c),1);
-        if(c != smatrix->order[i])
+        if (c != smatrix->order[i])
         {
             fprintf(stderr, "Datei hat falsches Format: "
                     "Die Reihenfolge der Aminosaueren stimmt nicht, "
                     "Matrix ist nicht symmetrisch\n");
             return(NULL);
         }
-        for(j = 0;j<i;j++)
+        for (j = 0;j<i;j++)
         {
             CHECK_READ(fscanf(fp," %d", &tmp),1);
-            if(tmp != smatrix->matrix[j][i-j])
+            if (tmp != smatrix->matrix[j][i-j])
             {
                 fprintf(stderr, "Datei hat falsches Format: "
                                 "Matrix ist nicht symmetrisch\n");
                 return(NULL);
             }
-        } 
-        for(; j < smatrix->dim; j++)
+        }
+        for (; j < smatrix->dim; j++)
         {
-            if(fscanf(fp," %d", &smatrix->matrix[i][j-i]) != 1)
+            if (fscanf(fp," %d", &smatrix->matrix[i][j-i]) != 1)
             {
                 fprintf(stderr, "Datei hat falsches Format: Die Zeilen "
                                 "sind nicht gleich lang\n");
@@ -87,9 +88,9 @@ Scorematrix *read_score(FILE *fp)
             }
         }
         CHECK_READ(fscanf(fp,"%c",&c),1);
-        while(c != '\n' || feof(fp) == 1)
+        while (c != '\n' || feof(fp) == 1)
         {
-            if(c != ' ')
+            if (c != ' ')
             {
                 fprintf(stderr, "Datei hat falsches Format: Die Zeilen "
                                 "sind nicht gleich lang\n");
@@ -101,62 +102,64 @@ Scorematrix *read_score(FILE *fp)
     return(smatrix);
 }
 
-long access_scorematrix(Scorematrix *smatrix, char aminoacid1, char aminoacid2)
+GtWord access_scorematrix(Scorematrix *smatrix,
+                          char aminoacid1,
+                          char aminoacid2)
 {
     int i,
-        pos1 = smatrix->dim, 
+        pos1 = smatrix->dim,
         pos2 = smatrix->dim;
-    long value;
-    assert(smatrix != NULL);
+    GtWord value;
+    gt_assert(smatrix != NULL);
 
-    for(i = 0; i < smatrix->dim; i++)
+    for (i = 0; i < smatrix->dim; i++)
     {
-        if(smatrix->order[i] == aminoacid1)
+        if (smatrix->order[i] == aminoacid1)
             pos1 = i;
-        if(smatrix->order[i] == aminoacid2)
+        if (smatrix->order[i] == aminoacid2)
             pos2 = i;
-        if(pos1 != smatrix->dim && pos2 != smatrix->dim)
+        if (pos1 != smatrix->dim && pos2 != smatrix->dim)
             break;
     }
-    if(pos1 == smatrix->dim)
+    if (pos1 == smatrix->dim)
     {
         printf("Aminosaeure: %c existiert in uebergegebner "
                         "Scorematrix nicht\n", aminoacid1);
         return(LONG_MAX);
     }
-    else if(pos2 == smatrix->dim)
+    else if (pos2 == smatrix->dim)
     {
         printf("Aminosaeure: %c existiert in uebergegebner "
                         "Scorematrix nicht\n", aminoacid2);
         return(LONG_MAX);
     }
-    if(pos1 < pos2)
+    if (pos1 < pos2)
         value = smatrix->matrix[pos1][pos2-pos1];
     else
         value = smatrix->matrix[pos2][pos1-pos2];
-        
+
     return(value);
 }
 
 int show_scorematrix(Scorematrix *smatrix)
 {
     unsigned i,j;
-    long score;
-    assert(smatrix != NULL);
+    GtWord score;
+    gt_assert(smatrix != NULL);
     printf("Die gespeicherte Scorematrix enthaelt folgende Werte:\n");
     printf("  ");
-    for(i=0; i<smatrix->dim; i++)
-    {
+    for (i = 0; i < smatrix->dim; i++)
         printf("%4c", smatrix->order[i]);
-    }
     printf("\n");
-    for(i=0; i<smatrix->dim; i++)
+    for (i = 0; i < smatrix->dim; i++)
     {
         printf("%c ", smatrix->order[i]);
-        for(j=0; j<smatrix->dim; j++)
+        for (j = 0; j < smatrix->dim; j++)
         {
-            score=access_scorematrix(smatrix, smatrix->order[i], smatrix->order[j]);
-            if(score == LONG_MAX)
+            score=access_scorematrix(smatrix,
+                                     smatrix->order[i],
+                                     smatrix->order[j]);
+            if (score == LONG_MAX)
                 return(EXIT_FAILURE);
             printf("%4ld", score);
         }
@@ -165,12 +168,12 @@ int show_scorematrix(Scorematrix *smatrix)
     return(EXIT_SUCCESS);
 }
 
-/* 
+/*
  * gibt allokierten Speicher wieder frei
  */
 void delete_scorematrix(Scorematrix *smatrix)
 {
-    if(smatrix!=NULL)
+    if (smatrix != NULL)
     {
         free(*(smatrix->matrix));
         free(smatrix->matrix);
