b57b09088b40e550c1d09b13348f1f992c328cdd
Hannah <2rauterb@informatik.uni-hamburg.de>
2015-09-10 15:58:41 +0200

    testing

--- testsuite/gt_sequencescorer_include.rb
+++ testsuite/gt_sequencescorer_include.rb
@@ -1,41 +1,79 @@
-Name "gt_sequencescorer_check_gramdistance_TransProt7"
+Name "gt sequencescorer Alphabet"
+Keywords "gt_sequencescorer scorer alphabet"
+Test do
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
+  run "#{$bin}gt encseq encode -smap TransProt11 #{$testdata}SequencescorerTwo.txt"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      " SequencescorerTwo.txt -q 4", :retval => 1
+end
+
+Name "gt sequencescorer Fileformat"
+Keywords "gt_sequencescorer scorer Fileformat"
+Test do
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -q 4", :retval => 1
+  run "#{$bin}gt scorer -qgram -ii -q 4", :retval => 1
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerOne.txt SequencescorerOne.txt -q 4", :retval => 1
+end
+
+Name "gt sequencescorer Qgramdistance"
+Keywords "gt_sequencescorer scorer qgram"
+Test do
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt -q 4", :retval => 0
+end
+
+Name "gt sequencescorer Qgramdistance TransProt7"
 Keywords "gt_sequencescorer scorer qgram TransProt7"
 Test do
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerTwo.txt"
-  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt SequencescorerTwo.txt -q 4"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -q 4"
   run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt7_q4.out"
 end
 
-Name "gt_sequencescorer_check_gramdistance_TransProt11"
+Name "gt sequencescorer Qgramdistance TransProt11"
 Keywords "gt_sequencescorer scorer qgram TransProt11"
 Test do
   run "#{$bin}gt encseq encode -smap TransProt11 #{$testdata}SequencescorerOne.txt"
   run "#{$bin}gt encseq encode -smap TransProt11 #{$testdata}SequencescorerTwo.txt"
-  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt SequencescorerTwo.txt -q 5"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -q 5"
   run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt11_q5.out"
 end
 
-Name "gt_sequencescorer_check_fscore_TransProt7"
+Name "gt sequencescorer Fscore"
+Keywords "gt_sequencescorer scorer fscore"
+Test do
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
+  run_test "#{$bin}gt scorer -fscore -ii SequencescorerOne.txt -k 6", :retval => 0
+end
+
+Name "gt sequencescorer Fscore TransProt7"
 Keywords "gt_sequencescorer scorer fscore TransProt7"
 Test do
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerTwo.txt"
-  run "#{$bin}gt scorer -fscore -ii SequencescorerOne.txt SequencescorerTwo.txt -k 4"
+  run "#{$bin}gt scorer -fscore -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -k 4"
   run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt7_k4.out"
   end
   
-Name "gt_sequencescorer_check_fscore_TransProt11"
+Name "gt sequencescorer Fscore TransProt11"
 Keywords "gt_sequencescorer scorer fscore TransProt11"
 Test do
   run "#{$bin}gt encseq encode -smap TransProt11 #{$testdata}SequencescorerOne.txt"
   run "#{$bin}gt encseq encode -smap TransProt11 #{$testdata}SequencescorerTwo.txt"  
-  run "#{$bin}gt scorer -fscore -ii SequencescorerOne.txt SequencescorerTwo.txt -k 5"
+  run "#{$bin}gt scorer -fscore -ii SequencescorerOne.txt "\
+     "SequencescorerTwo.txt -k 5"
   run "diff -B #{last_stdout} #{$testdata}sequencescorerCompare_TransProt11_k5.out"
 end 
 
 
-Name "gt_sequencescorer_check_gramdistance_lowerbound"
+Name "gt sequencescorer Qgramdistance lowerbound unitcost"
 Keywords "gt_sequencescorer scorer qgram lowerbound"
 Test do
   outfile = File.new("output.txt", "w")
@@ -60,7 +98,8 @@ Test do
   outfile.close
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
   run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerTwo.txt"
-  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt SequencescorerTwo.txt -q 4"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -q 4"
   outfile = File.new("#{last_stdout}", "r")
   qgramarray = outfile.readlines
   outfile.close
@@ -83,3 +122,98 @@ Test do
     end
   end
 end 
+
+=begin
+Name "gt sequencescorer Qgramdistance lowerbound blosom62"
+Keywords "gt_sequencescorer scorer qgram lowerbound"
+Test do
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerOne.txt"
+  run "#{$bin}gt encseq encode -smap TransProt7 #{$testdata}SequencescorerTwo.txt"
+  run "#{$bin}gt scorer -qgram -ii SequencescorerOne.txt "\
+      "SequencescorerTwo.txt -q 4"
+  outfile = File.new("#{last_stdout}", "r")
+  qgramarray = outfile.readlines
+  outfile.close
+  for i in 0...qgramarray.length
+    if(qgramarray[i][">"])
+        next
+    end
+    tmp = (qgramarray[i].split)
+    qgramarray.delete(qgramarray[i])
+    qgramarray.insert(i,tmp[tmp.length-1])
+  end
+  run "#{$bin}gt scorer -edist -ii SequencescorerOne.txt SequencescorerTwo.txt "\
+      "-smatrix #{$testdata}BLOSUM62.out -indelscore -1"
+  outfile = File.new("#{last_stdout}", "r")
+  edistarray = outfile.readlines
+  outfile.close
+  for i in 0...edistarray.length
+    if(edistarray[i][">"])
+        next
+    end
+    tmp = (edistarray[i].split)
+    edistarray.delete(edistarray[i])
+    edistarray.insert(i,tmp[tmp.length-1])
+  end
+  for i in 0...edistarray.length
+    if(("#{edistarray[i]}".to_i) < ("#{qgramarray[i]}".to_i)/8)
+      #Rethink! in what relation are the score an the qgramdistance? 
+    end
+  end
+end 
+=end
+
+Name "gt sequencescorer MaxMatches lowerbound unitcost"
+Keywords "gt_sequencescorer scorer maxmatches lowerbound"
+Test do
+  edist = File.new("calcEdist.txt", "w")
+  infile = File.new("#{$testdata}SequencescorerOne.txt", "r")
+  inarray = infile.readlines
+  infile.close
+  for j in 0...inarray.length
+    if(inarray[j][">"])
+      next
+    end
+    break
+  end
+  for i in j+1...inarray.length
+    if(inarray[i][">"])
+      next
+    end
+    run "#{$bin}gt dev paircmp -e -ss #{inarray[j].chomp} #{inarray[i].chomp}"
+    edist << last_stdout << "\n"
+  end
+  edist.close
+  firstseq = File.new("FirstSeq.txt", "w")
+  firstseq << inarray[0]
+  firstseq << inarray[1] << "\n"
+  firstseq.close
+  run "#{$bin}gt suffixerator -db FirstSeq.txt -suf -tis -smap TransProt7 "\
+      "-des no -md5 no -sds no"
+  run "#{$bin}gt scorer -maxmatches -ii FirstSeq.txt "\
+      "-seq #{$testdata}SequencescorerOne.txt"
+  maxmatches = File.new("#{last_stdout}", "r")
+  mmarray = maxmatches.readlines
+  maxmatches.close
+  for i in 0...mmarray.length
+    if(mmarray[i][">"])
+        next
+    end
+    if(mmarray[i]["#"])
+        next
+    end
+    tmp = (mmarray[i].split)
+    mmarray.delete(mmarray[i])
+    mmarray.insert(i,tmp[tmp.length-1])
+  end
+  edist = File.new("calcEdist.txt", "r")
+  edistarray = edist.readlines
+  edist.close
+  for i in 0...edistarray.length
+    tmp = File.new("#{edistarray[i].chomp}", "r")
+    tmparray = tmp.read.split()
+    if(!(("#{mmarray[i]}".to_i) <= "#{tmparray[0]}".to_i))
+      exit 1
+    end
+  end
+end
