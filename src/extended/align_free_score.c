b32377fbf5391740ede2384129333d096b69a78b
Hannah <2rauterb@informatik.uni-hamburg.de>
2015-09-14 16:45:02 +0200

    cleaning up

--- src/extended/align_free_score.c
+++ src/extended/align_free_score.c
@@ -1,7 +1,7 @@
 /*printf("\n Sequenz: "GT_WU"\n", gt_encseq_seqnum(encseq, 0));
 for(i = 0; i < (gt_encseq_total_length(encseq)); i++)
 {
-  if(gt_encseq_position_is_separator(encseq, i, GT_READMODE_FORWARD))
+  if (gt_encseq_position_is_separator(encseq, i, GT_READMODE_FORWARD))
   {
     printf("\n");
     printf("Sequenz: "GT_WU"\n", gt_encseq_seqnum(encseq, i+1));
@@ -11,6 +11,14 @@ for(i = 0; i < (gt_encseq_total_length(encseq)); i++)
 }
 printf("\n");*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include "match/esa-splititv.h"
+#include "match/sfx-mappedstr.h"
+#include "match/esa-map.h"
 #include "core/encseq_api.h"
 #include "core/str_api.h"
 #include "core/codetype.h"
@@ -19,107 +27,57 @@ printf("\n");*/
 #include "core/alphabet_api.h"
 #include "core/unused_api.h"
 #include "core/types_api.h"
-#include "match/esa-splititv.h"
-#include "match/sfx-mappedstr.h"
-#include "match/esa-map.h"
 #include "core/seq_iterator_sequence_buffer_api.h"
-
-#include "stdbool.h"
 #include "core/assert_api.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <math.h>
+#include "core/minmax.h"
+#include "core/array2dim_api.h"
 
-#include "score.h"
+#include "align_free_score.h"
 #include "scorematrix.h"
 
-#define SIZE 5
-#define MIN(A,B) ((A)<(B)?(A):(B))
-#define MAX(A,B) ((A)>(B)?(A):(B))
+typedef uint32_t GtKmercount;
 
-GtUword** new_tau(GtUword numofsequences, GtUword r, GtUword length)
+GtKmercount** kmer_profile_table_new(GtUword numofsequences,
+                                     GtUword r,
+                                     GtUword length)
 {
-  GtUword **tau,
-          i,
-          range;
-  range = (pow(r, length)+1);
-  tau = malloc((numofsequences)*sizeof(GtUword*));
-  *tau = calloc((numofsequences),range*sizeof(GtUword));
+  GtKmercount **tau;
+  GtUword i, range;
+  range = pow(r, length);
+  tau = gt_malloc((numofsequences)*sizeof(GtKmercount*));
+  *tau = gt_calloc((numofsequences),range*sizeof(GtKmercount));
   for (i = 1; i < numofsequences; i++)
+  {
     tau[i] = tau[i-1]+range;
+  }
   return tau;
 }
 
-void delete_tau(GtUword **tau)
+void kmer_profile_table_delete(GtKmercount **tau)
 {
   if (tau != NULL)
   {
-    free(*tau);
-    free(tau);
-  }
-}
-
-GtWord** new_table(GtUword seqlength_first, GtUword seqlength_second)
-{
-  GtUword i;
-  GtWord **table;
-  table = malloc((seqlength_first+1)*sizeof(GtWord*));
-  *table = malloc((seqlength_first+1)*(seqlength_second+1)*sizeof(GtWord));
-  for (i=1; i <= seqlength_first; i++)
-      table[i] = table[i-1]+(seqlength_second+1);
-  return table;
-}
-
-void delete_table(GtWord **table)
-{
-  if (table != NULL)
-  {
-    free(*table);
-    free(table);
+    gt_free(*tau);
+    gt_free(tau);
   }
 }
 
 void gt_get_codes(const GtEncseq *encseq,
                   GtUword len,
-                  GtUword **tau)
+                  GtKmercount **tau)
 {
-  GtUword i;
   GtKmercodeiterator *kc_iter;
   const GtKmercode *kmercode;
   GtUword seqnum,
           pos = 0;
   gt_assert(encseq != NULL);
-  for (i = 0; i < len; i++)
-  {
-    if (gt_encseq_total_length(encseq) <= pos)
-      return;
-    if (gt_encseq_position_is_separator(encseq, pos, GT_READMODE_FORWARD))
-    {
-      gt_warning("Sequence "GT_WU" is shorter than given k.\n",
-                 gt_encseq_seqnum(encseq, pos));
-      i = 0;
-      pos++;
-      continue;
-    }
-    pos++;
-  }
   kc_iter = gt_kmercodeiterator_encseq_new(encseq,
                                            GT_READMODE_FORWARD,
                                            len,
-                                           pos-len);
+                                           pos);
   while ((kmercode = gt_kmercodeiterator_encseq_next(kc_iter)) != NULL)
   {
     pos = (gt_kmercodeiterator_encseq_get_currentpos(kc_iter)-len);
-    if (gt_encseq_total_length(encseq) <= pos)
-      break;
-    if (gt_encseq_position_is_wildcard(encseq,
-                                       pos,
-                                       GT_READMODE_FORWARD))
-    {
-      gt_warning("Sequence "GT_WU" contains wildcards. Resulting scores"\
-                 "migth be wrong.\n", gt_encseq_seqnum(encseq, pos));
-    }
     if (!(kmercode->definedspecialposition))
     {
       seqnum = gt_encseq_seqnum(encseq, pos);
@@ -129,78 +87,70 @@ void gt_get_codes(const GtEncseq *encseq,
   gt_kmercodeiterator_delete(kc_iter);
 }
 
-Score *calc_qgram(GtEncseq *encseq_first,
-                           GtEncseq *encseq_second,
-                           GtUword r,
-                           GtUword q,
-                           GtError *err)
+Score *calc_qgram(const GtEncseq *encseq_first,
+                  const GtEncseq *encseq_second,
+                  GtUword r,
+                  GtUword q,
+                  GtError *err)
 {
-  gt_error_check(err);
-  gt_assert(encseq_first != NULL);
-  gt_assert(r > 0 && q > 0);
-
   GtUword numofseqfirst,
           numofseqsecond;
-  GtUword **tu,
-          **tv,
-          i, j, l;
-  float dist = 0;
+  GtKmercount **tu,
+              **tv;
+  GtUword i, j, l, range;
   Score *score;
+  bool compare = true;
 
-  numofseqfirst = gt_encseq_num_of_sequences(encseq_first);
-  tu = new_tau(numofseqfirst, r, q);
-  gt_get_codes(encseq_first, q, tu);
+  gt_error_check(err);
+  gt_assert(encseq_first != NULL && encseq_second != NULL && r > 0 && q > 0);
 
-  if (encseq_first && encseq_second)
+  if (encseq_first == encseq_second)
   {
-    numofseqsecond = gt_encseq_num_of_sequences(encseq_second);
-    tv = new_tau(numofseqsecond, r, q);
-    gt_get_codes(encseq_second, q, tv);
-
-    score = malloc(sizeof(Score)*((numofseqfirst*numofseqsecond)+1));
-    score->pos = 0;
-
-    for (i = 0; i < numofseqfirst; i++)
-    {
-      for (j = 0; j < numofseqsecond; j++)
-      {
-        dist = 0;
-        for (l = 0; l < pow(r,q); l++)
-        {
-          if (tu[i][l] > 0 || tv[j][l] > 0)
-            dist += abs(tu[i][l] - tv[j][l]);
-        }
-        score[score->pos].dist = dist;
-        score[score->pos].seqnum_u = i;
-        score[score->pos].seqnum_v = j;
-        score->pos++;
-      }
-    }
-    delete_tau(tv);
+    compare = false;
+  }
+  range = pow(r,q);
+  numofseqfirst = gt_encseq_num_of_sequences(encseq_first);
+  tu = kmer_profile_table_new(numofseqfirst, r, q);
+  gt_get_codes(encseq_first, q, tu);
+  if (!compare)
+  {
+    numofseqsecond = numofseqfirst;
+    tv = tu;
   }
   else
   {
-    score = malloc(sizeof(Score)*((numofseqfirst*numofseqfirst)+1));
-    score->pos = 0;
-
-    for (i = 0; i < numofseqfirst; i++)
+    numofseqsecond = gt_encseq_num_of_sequences(encseq_second);
+    tv = kmer_profile_table_new(numofseqsecond, r, q);
+    gt_get_codes(encseq_second, q, tv);
+  }
+  score = gt_malloc(sizeof(Score)*(numofseqfirst*numofseqsecond));
+  score->pos = 0;
+  for (i = 0; i < numofseqfirst; i++)
+  {
+    GtUword startidx = (encseq_first == encseq_second)? i+1 : 0;
+    for (j = startidx; j < numofseqsecond; j++)
     {
-      for (j = (i+1); j < numofseqfirst; j++)
+      double dist = 0;
+      for (l = 0; l < range; l++)
       {
-        dist = 0;
-        for (l = 0; l < pow(r,q); l++)
-        {
-          if (tu[i][l] > 0 || tu[j][l] > 0)
-             dist += abs(tu[i][l] - tu[j][l]);
-        }
-        score[score->pos].dist = dist;
-        score[score->pos].seqnum_u = i;
-        score[score->pos].seqnum_v = j;
-        score->pos++;
+        if (tu[i][l] > 0 || tv[j][l] > 0)
+          dist += abs(tu[i][l] - tv[j][l]);
       }
+      score[score->pos].dist = dist;
+      score[score->pos].seqnum_u = i;
+      score[score->pos].seqnum_v = j;
+      score->pos++;
     }
   }
-  delete_tau(tu);
+  if (!compare)
+  {
+    kmer_profile_table_delete(tu);
+  }
+  else
+  {
+    kmer_profile_table_delete(tv);
+    kmer_profile_table_delete(tu);
+  }
   return score;
 }
 
@@ -216,73 +166,63 @@ Score *calc_fscore(GtEncseq *encseq_first,
 
   GtUword numofseqfirst,
           numofseqsecond;
-  GtUword **tu,
-          **tv,
-          i, j, l,
+  GtKmercount **tu,
+              **tv;
+  GtUword i, j, l,
           tmp;
-  float dist = 0;
+  bool compare = true;
   Score *score;
 
-  numofseqfirst = gt_encseq_num_of_sequences(encseq_first);
-  tu = new_tau(numofseqfirst, r, k);
-  gt_get_codes(encseq_first, k, tu);
-
-  if (encseq_first && encseq_second)
+  if (encseq_first == encseq_second)
   {
-    numofseqsecond = gt_encseq_num_of_sequences(encseq_second);
-    tv = new_tau(numofseqsecond, r, k);
-    gt_get_codes(encseq_second, k, tv);
-
-    score = malloc(sizeof(Score)*((numofseqfirst*numofseqsecond)+1));
-    score->pos = 0;
-
-    for (i = 0; i < numofseqfirst; i++)
-    {
-      for (j = 0; j < numofseqsecond; j++)
-      {
-        tmp = 0;
-        for (l = 0; l < pow(r,k); l++)
-        {
-          if (tu[i][l] > 0 && tv[j][l] > 0)
-             tmp += MIN(tu[i][l],tv[j][l]);
-        }
-        GtUword length_u = gt_encseq_seqlength(encseq_first, i);
-        GtUword length_v = gt_encseq_seqlength(encseq_second, j);
-        dist = (float)tmp/(float)(MIN(length_u, length_v)-k+1);
-        score[score->pos].dist = dist;
-        score[score->pos].seqnum_u = i;
-        score[score->pos].seqnum_v = j;
-        score->pos++;
-      }
-    }
-    delete_tau(tv);
+    compare = false;
+  }
+  numofseqfirst = gt_encseq_num_of_sequences(encseq_first);
+  tu = kmer_profile_table_new(numofseqfirst, r, k);
+  gt_get_codes(encseq_first, k, tu);
+  if (!compare)
+  {
+    numofseqsecond = numofseqfirst;
+    tv = tu;
   }
   else
   {
-    score = malloc(sizeof(Score)*((numofseqfirst*numofseqfirst)+1));
-    score->pos = 0;
+    numofseqsecond = gt_encseq_num_of_sequences(encseq_second);
+    tv = kmer_profile_table_new(numofseqsecond, r, k);
+    gt_get_codes(encseq_second, k, tv);
+  }
+  score = gt_malloc(sizeof(Score)*(numofseqfirst*numofseqsecond));
+  score->pos = 0;
 
-    for (i = 0; i < numofseqfirst; i++)
+  for (i = 0; i < numofseqfirst; i++)
+  {
+    GtUword startidx = (encseq_first == encseq_second)? i+1 : 0;
+    for (j = startidx; j < numofseqsecond; j++)
     {
-      for (j = (i+1); j < numofseqfirst; j++)
+      tmp = 0;
+      for (l = 0; l < pow(r,k); l++)
       {
-        tmp = 0;
-        for (l = 0; l < pow(r,k); l++)
-        {
-          if (tu[i][l] > 0 && tu[j][l] > 0)
-             tmp += MIN(tu[i][l],tu[j][l]);
-        }
-        GtUword length_u = gt_encseq_seqlength(encseq_first, i);
-        GtUword length_v = gt_encseq_seqlength(encseq_first, j);
-        dist = (float)tmp/(float)(MIN(length_u, length_v)-k+1);
-        score[score->pos].dist = dist;
-        score[score->pos].seqnum_u = i;
-        score[score->pos].seqnum_v = j;
-        score->pos++;
+        if (tu[i][l] > 0 && tv[j][l] > 0)
+           tmp += MIN(tu[i][l],tv[j][l]);
       }
+      GtUword length_u = gt_encseq_seqlength(encseq_first, i);
+      GtUword length_v = gt_encseq_seqlength(encseq_second, j);
+      double dist = (double)tmp/(double)(MIN(length_u, length_v)-k+1);
+      score[score->pos].dist = dist;
+      score[score->pos].seqnum_u = i;
+      score[score->pos].seqnum_v = j;
+      score->pos++;
     }
   }
-  delete_tau(tu);
+  if (!compare)
+  {
+    kmer_profile_table_delete(tu);
+  }
+  else
+  {
+    kmer_profile_table_delete(tv);
+    kmer_profile_table_delete(tu);
+  }
   return score;
 }
 
@@ -294,7 +234,7 @@ Score *calc_edist(GtEncseq *encseq_first,
 {
   gt_error_check(err);
   gt_assert(encseq_first != NULL);
-  
+
   Scorematrix *smatrix;
   FILE *fp;
   GtUword numofseqfirst,
@@ -312,14 +252,14 @@ Score *calc_edist(GtEncseq *encseq_first,
   bool haserr = false;
 
   fp = fopen(gt_str_get(scorematrix), "r");
-  if(fp == NULL)
+  if (fp == NULL)
   {
     gt_error_set(err,"Scorematrixfile %s does not exist.\n",
                  gt_str_get(scorematrix));
     haserr = true;
   }
-  if(!haserr)
-  {  
+  if (!haserr)
+  {
     smatrix = read_score(fp);
     gt_assert(smatrix != NULL);
     fclose(fp);
@@ -341,7 +281,7 @@ Score *calc_edist(GtEncseq *encseq_first,
           m = gt_encseq_seqlength(encseq_first,u);
           n = gt_encseq_seqlength(encseq_second,v);
 
-          table = new_table(m, n);
+          gt_array2dim_malloc(table, m, n);
           table[0][0] = 0;
           for (i = 1; i <= m; i++)
             table[i][0] = table[i-1][0] + indelscore;
@@ -362,7 +302,7 @@ Score *calc_edist(GtEncseq *encseq_first,
               rep = table[i-1][j-1] + access_scorematrix(smatrix, one, two);
               tmp = MAX(ins, del);
               maximum = MAX(tmp, rep);
-              if(maximum == LONG_MAX)
+              if (maximum == LONG_MAX)
                 haserr = true;
               table[i][j] = maximum;
             }
@@ -371,7 +311,7 @@ Score *calc_edist(GtEncseq *encseq_first,
           score[score->pos].seqnum_u = u;
           score[score->pos].seqnum_v = v;
           score->pos++;
-          delete_table(table);
+          gt_array2dim_delete(table);
         }
       }
     }
@@ -389,7 +329,7 @@ Score *calc_edist(GtEncseq *encseq_first,
           m = gt_encseq_seqlength(encseq_first,u);
           n = gt_encseq_seqlength(encseq_first,v);
 
-          table = new_table(m, n);
+          gt_array2dim_malloc(table, m, n);
           table[0][0] = 0;
           for (i = 1; i <= m; i++)
             table[i][0] = table[i-1][0] + indelscore;
@@ -410,7 +350,7 @@ Score *calc_edist(GtEncseq *encseq_first,
               rep = table[i-1][j-1] + access_scorematrix(smatrix, one, two);
               tmp = MAX(ins, del);
               maximum = MAX(tmp, rep);
-              if(maximum == LONG_MAX)
+              if (maximum == LONG_MAX)
                 haserr = true;
               table[i][j] = maximum;
             }
@@ -419,7 +359,7 @@ Score *calc_edist(GtEncseq *encseq_first,
           score[score->pos].seqnum_u = u;
           score[score->pos].seqnum_v = v;
           score->pos++;
-          delete_table(table);
+          gt_array2dim_delete(table);
         }
       }
     }
@@ -452,14 +392,14 @@ void calc_maxmatches(GtStrArray *seq,
     gt_seq_iterator_set_symbolmap(seqit,
                                   gt_alphabet_symbolmap(gt_encseq_alphabet(
                                                         suffixarray->encseq)));
-    
+
     for (queryunitnum = 0; /* Nothing */; queryunitnum++)
     {
       retval = gt_seq_iterator_next(seqit,
                                     &query,
                                     &querylen,
                                     &desc,
-                                    err);                            
+                                    err);
       if (retval < 0)
       {
         haserr = true;
@@ -467,11 +407,11 @@ void calc_maxmatches(GtStrArray *seq,
       }
       if (retval == 0)
         break;
-      if(!haserr)
+      if (!haserr)
       {
         GtUword i = 0;
         score = 0;
-        while(i < querylen)
+        while (i < querylen)
         {
           totallength = gt_encseq_total_length(suffixarray->encseq);
           maxpreflength = gt_findmaximalprefixinESA(suffixarray->encseq,
@@ -480,15 +420,17 @@ void calc_maxmatches(GtStrArray *seq,
                                                     suffixarray->suftab,
                                                     &query[i],
                                                     querylen-i);
-          if(i == i + maxpreflength || maxpreflength == querylen)
+          /* i == i + maxpreflength <=> maxpreflength == 0 ? */
+          if (maxpreflength == 0 || maxpreflength == querylen)
+          {
             break;
+          }
           score++;
-          i = i + maxpreflength;
+          i = i + maxpreflength + 1; /* + 1? */
         }
         printf("Sequenz: "GT_WU"\tScore: "GT_WU"\n", queryunitnum, score);
-      }                                                
+      }
     }
     gt_seq_iterator_delete(seqit);
   }
 }
-
--- src/extended/align_free_score.h
+++ src/extended/align_free_score.h
@@ -1,5 +1,5 @@
-#ifndef SCORE_H
-#define SCORE_H
+#ifndef ALIGN_FREE_SCORE_H
+#define ALIGN_FREE_SCORE_H
 
 typedef struct{
   GtUword seqnum_u,
@@ -14,8 +14,8 @@ Score* calc_fscore(GtEncseq *encseq_first,
                    GtUword k,
                    GtError *err);
 
-Score* calc_qgram(GtEncseq *encseq_first,
-                  GtEncseq *encseq_second,
+Score *calc_qgram(const GtEncseq *encseq_first,
+                  const GtEncseq *encseq_second,
                   GtUword r,
                   GtUword q,
                   GtError *err);
@@ -25,7 +25,7 @@ Score *calc_edist(GtEncseq *encseq_first,
                   GtStr *scorematrix,
                   int indelscore,
                   GT_UNUSED GtError *err);
-                  
+
 void calc_maxmatches(GtStrArray *seq,
                      Suffixarray *suffixarray,
                      GtError *err);
